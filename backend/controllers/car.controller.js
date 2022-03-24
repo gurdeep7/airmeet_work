02c8513f251d94fc308b45f4a572c33d4170232e
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2022-03-24 11:14:07 +0530

    update

--- backend/controllers/car.controller.js
+++ backend/controllers/car.controller.js
@@ -6,12 +6,11 @@ const router = express.Router()
 router.post("",async(req,res)=>{
     try {
        
-         const cars =await car.create(req.body)
+    const cars =await car.create(req.body)
  
-     res.status(200).send(cars)
+    res.status(200).send(cars)
  }catch(e){
- 
-     res.status(500).json({status:e.message})
+  res.status(500).json({status:e.message})
  }
  })
 
