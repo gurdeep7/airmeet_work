8e80d2d84a809f4fae45a3500df4c270070a3bda
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2022-03-24 19:05:15 +0530

    update style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,10 +16,10 @@ return(<div><div className="flex bg-lime-500 p-6">
 <p className="text-7xl m-auto animate-ping"> ITEM LISTS </p>
 </div>
 
-<div className="text-left flex">
+<div className="text-left flex bg-purple-200">
     <button className="m-4 w-1/5 bg-yellow-100 border-2 border-black hover:bg-yellow-500" onClick={handleFavourite}>Mark selected as Favourite</button>
     <button className="m-4 w-1/5 bg-red-300 border-2 border-black hover:bg-red-800"> Delete Selected items</button>
-    <div className="m-4 w-1/5">Current page is {page}</div>
+    <div className="m-4 w-1/5 text-3xl bg-pink-200">Current page is {page}</div>
 </div>
 </div>)
 }
