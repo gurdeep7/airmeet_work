c8597d9055c76b9cd28c3c6058075ca2e7cbfbe9
gurdeep7 <90665311+gurdeep7@users.noreply.github.com>
2022-06-05 13:00:49 +0530

    testing

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,37 @@
 const express = require("express")
 
+const cookieParser = require('cookie-parser')
+
 const app = express()
 
 const carController = require("./controllers/car.controller")
 
 const cors = require("cors")
+const { v4 } = require("uuid")
+const session = require("express-session")
+const { default: helmet } = require("helmet")
 
 app.use(cors())
 
+app.use(cookieParser())
+
 app.use(express.json())
 
+app.use(helmet())
+
+app.use(session({
+    genid: function(req) {
+      return v4() // use UUIDs for session IDs
+    },
+    
+    proxy: true,
+    secret: 'xgZyQK',
+    rolling:true,
+   cookie:{ httpOnly: true, sameSite:"none", secure:true, maxAge:20 * 60000} //20 minutes
+  
+  }))
+
+ 
 app.get("/",(req,res)=>{
     res.send("Working Backend")
 })
